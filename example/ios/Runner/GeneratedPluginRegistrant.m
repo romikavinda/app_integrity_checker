83bcb72b695e0245d66e3ad900c4f9d221d83cdd
romikavinda <romikavinda@yahoo.com>
2023-12-05 15:08:45 +0530

    Add files via upload

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<app_integrity_checker/AppIntegrityCheckerPlugin.h>)
+#import <app_integrity_checker/AppIntegrityCheckerPlugin.h>
+#else
+@import app_integrity_checker;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AppIntegrityCheckerPlugin registerWithRegistrar:[registry registrarForPlugin:@"AppIntegrityCheckerPlugin"]];
+}
+
+@end
