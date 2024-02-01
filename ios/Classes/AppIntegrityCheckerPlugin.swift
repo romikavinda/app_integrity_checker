b52337c60ac22a55b1560b7517f5c69d84e7fcc2
romikavinda <romikavinda@yahoo.com>
2024-02-01 10:01:37 +0530

    Update AppIntegrityCheckerPlugin.swift

--- ios/Classes/AppIntegrityCheckerPlugin.swift
+++ ios/Classes/AppIntegrityCheckerPlugin.swift
@@ -14,41 +14,39 @@ public class AppIntegrityCheckerPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-            case "getchecksum":
-                let hashvalue3 = checkDartCode()
-                let hashValue = IOSSecuritySuite.getMachOFileHashValue(.default)
-                let hashValue2 = IOSSecuritySuite.getMachOFileHashValue(.custom("IOSSecuritySuite"))
-                let finvalue = hashvalue3.prefix(8) + hashValue!.prefix(8) + hashValue2!.prefix(8)
-                result(finvalue)
-                break
-            case "getsig":
-                let sigvalue = checkCodeSignature()
-                result(sigvalue)
-                break
-            default:
-                result(FlutterMethodNotImplemented)
+    case "getchecksum":
+        let hashval = getHashVal()
+        result(hashval)
+      break
+    case "getsig":
+        let sigvalue = checkCodeSignature()
+        result(sigvalue)
+      break
+    default:
+        result(FlutterMethodNotImplemented)
     }
-
   }
-
 }
 
-  public func checkDartCode() -> String {
-      guard let url = Bundle.main.url(forResource: "App", withExtension:nil,subdirectory: "Frameworks/App.framework") else{ return "" }
-      if FileManager.default.fileExists(atPath: url.path) {
-          if let data = FileManager.default.contents(atPath: url.path) {
-              var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-              data.withUnsafeBytes {
-                  _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
-              }
 
-              return Data(hash).hexEncodedString()
+public func getHashVal() -> String{
+    let hashvalue3 = checkDartCode()
+#if arch(arm64)
+    let hashValue = IOSSecuritySuite.getMachOFileHashValue(.default)
+    let hashValue2 = IOSSecuritySuite.getMachOFileHashValue(.custom("IOSSecuritySuite"))
+    let finvalue = hashvalue3.prefix(8) + hashValue!.prefix(8) + hashValue2!.prefix(8)
+    return String(finvalue);
+    
+#endif
+    return String(hashvalue3.prefix(8));
+}
 
-          }
-      }
 
-      return ""
-  }
+public func getHash86() -> String{
+    let hashvalue3 = checkDartCode()
+    let finvalue = hashvalue3.prefix(8)
+    return String(finvalue);
+}
 
     public func checkCodeSignature() -> String {
         guard let url = Bundle.main.url(forResource: "CodeResources", withExtension:nil,subdirectory: "_CodeSignature") else{ return "" }
@@ -67,27 +65,25 @@ public class AppIntegrityCheckerPlugin: NSObject, FlutterPlugin {
         return ""
     }
 
+      public func checkDartCode() -> String {
+          guard let url = Bundle.main.url(forResource: "App", withExtension:nil,subdirectory: "Frameworks/App.framework") else{ return "" }
+          if FileManager.default.fileExists(atPath: url.path) {
+              if let data = FileManager.default.contents(atPath: url.path) {
+                  var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+                  data.withUnsafeBytes {
+                      _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
+                  }
 
-/*     public func checkmobile() -> String {
-        guard let url = Bundle.main.url(forResource: "embedded", withExtension:"mobileprovision") else{ return "" }
-        if FileManager.default.fileExists(atPath: url.path) {
-            if let data = FileManager.default.contents(atPath: url.path) {
-                var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-                data.withUnsafeBytes {
-                    _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
-                }
+                  return Data(hash).hexEncodedString()
 
-                return Data(hash).hexEncodedString()
+              }
+          }
 
-            }
-        }
-
-        return ""
-    } */
-
-
-  extension Data {
-      fileprivate func hexEncodedString() -> String {
-          return map { String(format: "%02hhx", $0) }.joined()
+          return ""
       }
-  }
+
+       extension Data {
+           fileprivate func hexEncodedString() -> String {
+               return map { String(format: "%02hhx", $0) }.joined()
+           }
+       }
